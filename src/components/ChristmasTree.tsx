a66291153f80bb7ec88d4254699062c701d68af8
leeye51456 <leeye51456@gmail.com>
2021-12-19 23:40:22 +0900

    Change `ChristmasTree` to an SVG element

--- src/components/ChristmasTree.tsx
+++ src/components/ChristmasTree.tsx
@@ -1,11 +1,22 @@
 function ChristmasTree() {
   return (
-    <div>
-      <div>
+    <svg
+      viewBox="0 0 1000 1000"
+    >
+      <text
+        x="50%"
+        y="50%"
+        dominant-baseline="central"
+        text-anchor="middle"
+        style={{
+          fontSize: '800px',
+          lineHeight: 1,
+        }}
+      >
         🌲
-      </div>
+      </text>
       {/* <Ornaments /> */}
-    </div>
+    </svg>
   )
 }
 
