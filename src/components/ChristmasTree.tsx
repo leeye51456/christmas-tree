352c2e145f21b5cac00bb7a34b7b69f413399cb0
leeye51456 <leeye51456@gmail.com>
2021-12-23 00:49:30 +0900

    Add ornaments a star and "Merry Christmas"

--- src/components/ChristmasTree.tsx
+++ src/components/ChristmasTree.tsx
@@ -1,3 +1,5 @@
+import Ornament from "./svg/Ornament"
+
 function ChristmasTree() {
   return (
     <svg
@@ -15,7 +17,19 @@ function ChristmasTree() {
       >
         &#x1f332;
       </text>
-      {/* <Ornaments /> */}
+
+      <Ornament top={0} r={0} size={15}>
+        &#x1f31f;
+      </Ornament>
+
+      <Ornament top={17.5} r={0} size={7.5} style={{ fill: '#e9d496', letterSpacing: '-0.2em' }} >
+        <tspan x="50%" dy="-0.8em">
+          &#x1d4dc;&#x1d4ee;&#x1d4fb;&#x1d4fb;&#x1d502;
+        </tspan>
+        <tspan x="50%" dy="0.8em">
+          &#x1d4d2;&#x1d4f1;&#x1d4fb;&#x1d4f2;&#x1d4fc;&#x1d4fd;&#x1d4f6;&#x1d4ea;&#x1d4fc;
+        </tspan>
+      </Ornament>
     </svg>
   )
 }
